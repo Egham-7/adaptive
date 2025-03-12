5877aa63ec730653e506d14c40341d0d0755bfa4
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-03-12 22:11:39 +0000

    feat(home-layout): add HomeLayoutSkeleton component for loading state representation feat(home-layout): implement loading state in HomeLayout to show skeleton while auth is loading fix(home-layout): redirect to home if user is not signed in after auth check

--- adaptive-frontend/src/layouts/home-layout.tsx
+++ adaptive-frontend/src/layouts/home-layout.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useRouter } from "@tanstack/react-router";
 import { ModeToggle } from "@/components/mode-toggle";
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import { ChatbotSidebar } from "@/components/chatbot-sidebar";
+import { useAuth } from "@clerk/clerk-react";
+import { HomeLayoutSkeleton } from "@/components/skeletons/home-layout-skeleton";
 
 export function HomeLayout() {
   const { open, openMobile } = useSidebar();
+
+  const { isLoaded, isSignedIn } = useAuth();
+
+  const { navigate } = useRouter();
+
+  if (!isLoaded) {
+    return <HomeLayoutSkeleton />;
+  }
+
+  if (!isSignedIn) {
+    navigate({ to: "/" });
+  }
+
   return (
     <div className="min-h-screen bg-background flex w-full">
       <ChatbotSidebar />
