7732f651bc2a7001a8b2c0ffb4d3a8d99741853b
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-03-16 21:14:14 +0000

    feat(api): add authentication middleware to API routes for enhanced security feat(api): implement authentication token handling in API key hooks for secure API interactions refactor(api): update API key service functions to accept authentication tokens for secure requests

--- adaptive-frontend/src/lib/hooks/api_keys/use-get-api-key-id.ts
+++ adaptive-frontend/src/lib/hooks/api_keys/use-get-api-key-id.ts
@@ -1,11 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAPIKeyById } from "@/services/api_keys";
 import { APIKey } from "@/services/api_keys/types";
+import { useAuth } from "@clerk/clerk-react";
 
 export const useGetAPIKeyById = (id: string) => {
+  const { getToken, isSignedIn, isLoaded } = useAuth();
   return useQuery<APIKey, Error>({
     queryKey: ["apiKey", id],
-    queryFn: () => getAPIKeyById(id),
+    queryFn: async () => {
+      if (!isSignedIn || !isLoaded) {
+        throw new Error("User is not signed in");
+      }
+
+      const token = await getToken();
+
+      if (!token) {
+        throw new Error("User is not signed in");
+      }
+
+      return getAPIKeyById(id, token);
+    },
     enabled: !!id,
   });
 };
