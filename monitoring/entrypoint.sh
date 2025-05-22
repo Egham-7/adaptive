ff07e4d66b5ab0cbed877a4aec105456aa3bea01
Botir Khaltaev <botir.khaltaev@squaredup.com>
2025-05-23 00:00:13 +0100

    Fix Prometheus deployment configuration
    Add explicit config

--- monitoring/entrypoint.sh
+++ monitoring/entrypoint.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-envsubst </etc/prometheus/prometheus.yml.tmpl >/etc/prometheus/prometheus.yml
-exec /bin/prometheus "$@"
+#!/bin/bash
+
+# Replace environment variables in the configuration
+envsubst < /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
+
+# Start Prometheus with the generated config
+exec /bin/prometheus --config.file=/etc/prometheus/prometheus.yml
