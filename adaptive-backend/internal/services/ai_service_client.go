ec0d2178c57bf419fe7925720ad2e7d478b38acf
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-05-23 16:48:33 +0100

    fix: monitoring:

--- adaptive-backend/internal/services/ai_service_client.go
+++ adaptive-backend/internal/services/ai_service_client.go
@@ -25,7 +25,7 @@ func NewPromptClassifierClient() *PromptClassifierClient {
 
 func (c *PromptClassifierClient) SelectModel(prompt string) (*models.SelectModelResponse, error) {
 	var result models.SelectModelResponse
-	err := c.client.Post("/api/model/select-model", models.SelectModelRequest{Prompt: prompt}, &result, &RequestOptions{Timeout: 50 * time.Second})
+	err := c.client.Post("/predict", models.SelectModelRequest{Prompt: prompt}, &result, &RequestOptions{Timeout: 50 * time.Second})
 
 	return &result, err
 }
