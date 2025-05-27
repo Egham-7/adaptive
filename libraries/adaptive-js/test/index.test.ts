6d1660e9d8f121d7e41b6d52d2e0d67f899f4085
Botir Khaltaev <btrghstk@gmail.com>
2025-05-27 14:31:52 +0100

    feat: change for dummy tests

--- libraries/adaptive-js/test/index.test.ts
+++ libraries/adaptive-js/test/index.test.ts
@@ -1,12 +1,7 @@
-import { describe, it, expect } from 'bun:test'
-import { one, two } from '../src'
+import { describe, it, expect } from "bun:test";
 
-describe('should', () => {
-  it('export 1', () => {
-    expect(one).toBe(1)
-  })
-
-  it('export 2', () => {
-    expect(two).toBe(2)
-  })
-})
+describe("always passes", () => {
+  it("dummy test 1", () => {
+    expect(true).toBe(true);
+  });
+});
