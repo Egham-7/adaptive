86e154afa1204a064f0154fab576e37d1ff36055
Botir Khaltaev <btrghstk@gmail.com>
2025-06-02 12:55:03 +0100

    fix: remove auth middleware globally

--- adaptive-backend/cmd/api/main.go
+++ adaptive-backend/cmd/api/main.go
@@ -133,5 +133,4 @@ func setupMiddleware(app *fiber.App, allowedOrigins string) {
 	prometheus := fiberprometheus.New("adaptive-backend")
 	prometheus.RegisterAt(app, "/metrics")
 	app.Use(prometheus.Middleware)
-	app.Use(middleware.AuthMiddleware())
 }
