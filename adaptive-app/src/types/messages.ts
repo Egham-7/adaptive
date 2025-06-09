cab6a7d8646e4871c695ccd63cbbe642a28fb0ac
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-06-09 12:57:53 +0100

    feat: migrate frontend to other repo

--- adaptive-app/src/types/messages.ts
+++
@@ -1,7 +0,0 @@
-import type { RouterInputs, RouterOutputs } from "@/trpc/react";
-
-export type Message = RouterOutputs["messages"]["getById"];
-export type MessageCreateInput = RouterInputs["messages"]["create"];
-export type MessageUpdateInput = RouterInputs["messages"]["update"];
-export type MessageListInput = RouterInputs["messages"]["listByConversation"];
-export type MessageBatchUpsertInput = RouterInputs["messages"]["batchUpsert"];
