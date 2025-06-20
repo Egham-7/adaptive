e37a1e7c42276e7646feea3818655dc279bc50c6
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-06-20 19:11:39 +0100

    chore(Dockerfile): copy prisma folder and update command to deploy migrations before starting the application

--- adaptive-app/prisma/generated/edge.js
+++ adaptive-app/prisma/generated/edge.js
@@ -203,8 +203,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "..",
   "clientVersion": "6.10.0",
--- adaptive-app/prisma/generated/index.js
+++ adaptive-app/prisma/generated/index.js
@@ -204,8 +204,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "..",
   "clientVersion": "6.10.0",
