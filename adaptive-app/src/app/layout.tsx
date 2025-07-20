5d70986b7636087f951246281a6e316ad2fea04d
Botir Khaltaev <botirkhaltaev@llmadaptive.uk>
2025-07-20 23:39:51 +0100

    chore: format

--- adaptive-app/src/app/layout.tsx
+++ adaptive-app/src/app/layout.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
-import { Lora, Outfit, Roboto_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { ThemeProvider } from "@/context/theme-provider";
@@ -10,39 +9,39 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { HydrateClient } from "@/trpc/server";
 
 export const metadata: Metadata = {
-  title: "Adaptive",
-  description: "Adaptive",
+	title: "Adaptive",
+	description: "Adaptive",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <TRPCReactProvider>
-              <HydrateClient>
-                <SidebarProvider>
-                  <div className="min-h-screen w-full bg-background">
-                    {children}
-                  </div>
-                </SidebarProvider>
-              </HydrateClient>
-            </TRPCReactProvider>
-          </ThemeProvider>
-          <Analytics />
-          <Toaster />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<TRPCReactProvider>
+							<HydrateClient>
+								<SidebarProvider>
+									<div className="min-h-screen w-full bg-background">
+										{children}
+									</div>
+								</SidebarProvider>
+							</HydrateClient>
+						</TRPCReactProvider>
+					</ThemeProvider>
+					<Analytics />
+					<Toaster />
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
