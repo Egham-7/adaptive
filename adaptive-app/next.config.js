8c3fd631cae8e3946bfd3cae628ca7bdcfe50a94
Botir Khaltaev <btrghstk@gmail.com>
2025-07-29 11:22:19 +0100

    fix: use only es module syntax

--- adaptive-app/next.config.js
+++ adaptive-app/next.config.js
@@ -25,13 +25,11 @@ const config = {
 	},
 };
 
-export default config;
-
 // Injected content via Sentry wizard below
 
-const { withSentryConfig } = require("@sentry/nextjs");
+import { withSentryConfig } from "@sentry/nextjs";
 
-module.exports = withSentryConfig(module.exports, {
+export default withSentryConfig(config, {
 	// For all available options, see:
 	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
