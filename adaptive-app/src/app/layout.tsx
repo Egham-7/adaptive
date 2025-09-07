c5f6e426592a2bbccfc989375418edbfb460f9f9
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-09-07 23:44:16 +0100

    style(layout.tsx): format code for better readability and consistency in indentation and spacing

--- adaptive-app/src/app/layout.tsx
+++ adaptive-app/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Toaster } from "sonner";
 import { SidebarProvider } from "@/components/ui/sidebar";
@@ -9,39 +8,38 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { HydrateClient } from "@/trpc/server";
 
 export const metadata: Metadata = {
-	title: "Adaptive",
-	description: "Adaptive",
+  title: "Adaptive",
+  description: "Adaptive",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<ClerkProvider>
-			<html lang="en" suppressHydrationWarning>
-				<body>
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange
-					>
-						<TRPCReactProvider>
-							<HydrateClient>
-								<SidebarProvider>
-									<div className="min-h-screen w-full bg-background">
-										{children}
-									</div>
-								</SidebarProvider>
-							</HydrateClient>
-						</TRPCReactProvider>
-					</ThemeProvider>
-					<Analytics />
-					<Toaster />
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TRPCReactProvider>
+              <HydrateClient>
+                <SidebarProvider>
+                  <div className="min-h-screen w-full bg-background">
+                    {children}
+                  </div>
+                </SidebarProvider>
+              </HydrateClient>
+            </TRPCReactProvider>
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
