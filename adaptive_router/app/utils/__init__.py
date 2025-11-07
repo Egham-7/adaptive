f5851626dcb9047eea48f5285abee9548deab639
Botir Khaltaev <btrghstk@gmail.com>
2025-11-07 19:06:30 +0000

    feat: remove fuzzy matching from router

--- adaptive_router/app/utils/__init__.py
+++ adaptive_router/app/utils/__init__.py
@@ -1,6 +1,5 @@
 """Utility functions for the Adaptive Router application."""
 
-from app.utils.fuzzy_matching import enhance_model_costs_with_fuzzy_keys
 from app.utils.model_resolver import resolve_models
 
-__all__ = ["enhance_model_costs_with_fuzzy_keys", "resolve_models"]
+__all__ = ["resolve_models"]
