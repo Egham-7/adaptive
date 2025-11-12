18eba4316b78bbd45982e8ee3129637f3b43053b
Botir Khaltaev <btrghstk@gmail.com>
2025-11-12 20:01:23 +0000

    feat: add dataset module in adaptive router

---
+++ adaptive_router/adaptive_router/dataset/__init__.py
@@ -0,0 +1,10 @@
+from deepeval.contextvars import get_current_golden
+from deepeval.dataset import EvaluationDataset, Golden, ConversationalGolden
+
+
+__all__ = [
+    "EvaluationDataset",
+    "Golden",
+    "ConversationalGolden",
+    "get_current_golden",
+]
